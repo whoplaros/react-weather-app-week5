4ca1a92a80336c85d5441e6c745c954e1bb2d05d
Whoplaros <whoplaros@gmail.com>
2025-01-05 17:26:29 +0200

    added celsius and fahrenheit switcher

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate.js";
 import WeatherIcon from "./WeatherIcon.js";
+import WeatherTemperature from "./WeatherTemperature.js";
 
 export default function WeatherInfo(props) {
 	return (
@@ -19,10 +20,7 @@ export default function WeatherInfo(props) {
 							<WeatherIcon code={props.data.icon} />
 						</div>
 						<div className="float-start">
-							<span className="temperature">
-								{Math.round(props.data.temperature)}
-							</span>
-							<span className="unit">°C</span>
+							<WeatherTemperature celsius={props.data.temperature} />
 						</div>
 					</div>
 				</div>
