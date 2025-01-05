46d38b82ec78f767e7ffb4d6ec752147fa8be03f
Whoplaros <whoplaros@gmail.com>
2025-01-05 12:11:22 +0200

    added basic styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
+import Weather from "./Weather.js";
 
 function App() {
 	return (
 		<div className="App">
 			<div className="container">
 				<h1>Weather App</h1>
-				<button className="btn btn-primary">Hello</button>
+				<Weather />
+
 				<footer>
 					This project was coded by{" "}
 					<a href="https://wesleyhoplarosportfolio.netlify.app" target="_blank">
