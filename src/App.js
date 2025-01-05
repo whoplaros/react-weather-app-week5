6f73a82d962f453a54fffeaa062c0552024f94eb
Whoplaros <whoplaros@gmail.com>
2025-01-05 10:45:04 +0200

    added github repo url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
 
 function App() {
-	return <div className="App">Weather App</div>;
+	return (
+		<div className="App">
+			<h1>Weather App</h1>
+			<footer>
+				This project was coded by Wesley Hoplaros and is open-sourced on{" "}
+				<a
+					href="https://github.com/whoplaros/react-weather-app-week5"
+					target="_blank"
+				>
+					Github.
+				</a>
+			</footer>
+		</div>
+	);
 }
 
 export default App;
