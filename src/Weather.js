4ad17fcf6d5104438516077792d23e2a2e03359f
Whoplaros <whoplaros@gmail.com>
2025-01-05 13:28:03 +0200

    added Weather props

--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
+export default function Weather(props) {
 	const [weatherData, setWeatherData] = useState({ ready: false });
 
 	function handleResponse(response) {
@@ -73,8 +73,7 @@ export default function Weather() {
 		);
 	} else {
 		const apiKey = "e546fd9teb385774339o4d3b5b79c3a0";
-		let city = "New York";
-		let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+		let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=metric`;
 		axios.get(apiUrl).then(handleResponse);
 		return "Loading...";
 	}
