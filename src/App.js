4675a730ecb43bfaca44be1a4ed30ab32be83e07
Whoplaros <whoplaros@gmail.com>
2025-01-05 17:37:02 +0200

    security bug fix

--- src/App.js
+++ src/App.js
@@ -10,13 +10,18 @@ function App() {
 
 				<footer>
 					This project was coded by{" "}
-					<a href="https://wesleyhoplarosportfolio.netlify.app" target="_blank">
+					<a
+						href="https://wesleyhoplarosportfolio.netlify.app"
+						target="_blank"
+						rel="noreferrer"
+					>
 						Wesley Hoplaros
 					</a>{" "}
 					and is open-sourced on{" "}
 					<a
 						href="https://github.com/whoplaros/react-weather-app-week5"
 						target="_blank"
+						rel="noreferrer"
 					>
 						Github
 					</a>{" "}
@@ -24,6 +29,7 @@ function App() {
 					<a
 						href="https://asyouwish-weather-react-app5.netlify.app"
 						target="_blank"
+						rel="noreferrer"
 					>
 						Netlify
 					</a>
